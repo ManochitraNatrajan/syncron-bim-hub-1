c1d3897e15dcc398aee8a15bd6a042d49c3e31cf
ManochitraNatrajan <manochitra.mages@gmail.com>
2026-02-08 20:12:11 +0530

    Update code across all pages

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/syncron-bim-hub-1/', // <-- your repository name here
   plugins: [react()],
 })
